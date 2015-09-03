4161dd7e1a6c8eeee43a361ed07fca66104a1d7b
Malcolm Byrd <malcolmbyrd@users.noreply.github.com>
2015-09-03 11:36:38 -0400

    added documentation

--- scripts/myList.sh
+++ scripts/myList.sh
@@ -1,2 +1,7 @@
+# Team 'Isn't This Fun' 
+# Zach Davis, Ben Byrd, Kaitlyn Fulford, Adam Sugarman
+
+# This script lists the contents in its containing directory in an HTML file in the browser.
+
 ls > directoryOutput.html           #list top level-directory contents and redirect the to a html file directoryOutput.html
 xdg-open directoryOutput.html &     #open the directoryOutput.html file (without causing the terminal to wait)
