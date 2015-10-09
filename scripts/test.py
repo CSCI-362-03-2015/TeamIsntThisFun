2be8c19fe2c996e84f6306df2f41c1753f4d5dff
Zack Davis <todzzx@gmail.com>
2015-10-09 14:40:03 -0400

    added lots of code. readFiles, parseFiles, driver for now. It has an error (each file isn't coming in in order), but here it is for now.

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,11 @@
 import sys
 import os
+from beets import ui
+#from beets.ui import human_bytes
+
+# Full script plan:
+#   Read file (populate array) -> Parse file -> Sent to driver, calls functions from parse
+#   -> Output to report function -> Html
 
 def main():
   # todo: import 5 beets functions or all of beets if necessary
@@ -8,22 +14,60 @@ def main():
   # by this point we have imported all beets function necessary
 
   # walk the tree
+  readFiles()
+
+def readFiles():
+
   rootDir = '../testCases/'
 
   for filename in os.listdir(rootDir):
 
     with open(os.path.join(rootDir, filename), "r") as f:
-      print(filename);
+      #print(filename);
+      lineArray = []
+
       for line in f:
         # helper function here to populate an array of the 6 lines
-        print(line);
+        lineArray.append(line)
+        print(line)
+      #driver(parseFiles(lineArray))
 
-        # helper function here to call the beets function with input
+def parseFiles(inLineArray):
+  #1. test number or ID
+  #2. requirement being tested
+  #3. component being tested
+  #4. method being tested
+  #5. test input(s) including command-line argument(s)
+  #6. expected outcome(s)
 
-        # helper function to compare results
+  paren = '('
+  comm = '#'
 
-        # helper function to pass output to html file
+  head, mid, tail = inLineArray[3].partition(paren)
+  funcName = head
 
+  print(inLineArray[4])
+  if '#' in inLineArray[4]:
+    head, mid, tail = inLineArray[4].partition(comm)
+  else:
+    head = inLineArray[4]
+
+  try:
+    inputVal = float(head.strip())
+  except:
+    print(head)
+    inputVal = 1.0
+
+  returnVal = [funcName, inputVal]
+  return returnVal
+
+
+def driver(info):
+  inFuncName = info[0]
+  inInputVal = info[1]
+
+  output = getattr(ui, inFuncName)(inInputVal)
+  print(output)
 
 if __name__ == "__main__":
   main()
