79cf8c838b2d6906f7a85295a6b4ff8cb5302718
Zack Davis <todzzx@gmail.com>
2015-10-21 18:24:35 -0400

    Did parse, temporarily only does first testCase.

--- scripts/test.py
+++ scripts/test.py
@@ -1,96 +1,91 @@
 import sys
 import os
 from beets import ui
-#from beets.ui import human_bytes
+
+
+# from beets.ui import human_bytes
 
 # Full script plan:
 #   Read file (populate array) -> Parse file -> Sent to driver, calls functions from parse
 #   -> Output to report function -> Html
 
 def main():
-  # todo: import 5 beets functions or all of beets if necessary
-  # todo: write 4 helper functions: open file, run the method, compare results, write to html output
+    # todo: import 5 beets functions or all of beets if necessary
+    # todo: write 4 helper functions: open file, run the method, compare results, write to html output
 
-  # by this point we have imported all beets function necessary
+    # by this point we have imported all beets function necessary
 
-  # walk the tree
-  readFiles()
+    # walk the tree
+    readFiles()
+
 
 def readFiles():
+    rootDir = '../testCases/'
 
-  rootDir = '../testCases/'
+    count = 0
 
-  for filename in os.listdir(rootDir):
+    for filename in os.listdir(rootDir):
+        with open(os.path.join(rootDir, filename), "r") as f:
+            # infoLines is an array populated by the first six lines of the testCase file
+            infoLines = f.readlines()[0:6]
+            infoLines = list(map(str.strip, infoLines))
 
-    with open(os.path.join(rootDir, filename), "r") as f:
-      #print(filename);
-      lineArray = []
+            driver(parseFiles(infoLines))
+            return 0
 
-      for line in f:
-        # helper function here to populate an array of the 6 lines
-        lineArray.append(line)
-        #print(line)
-        #print
-      #driver(parseFiles(lineArray))
-
-  for filename in os.listdir(rootDir):
-
-    with open(os.path.join(rootDir, filename), "r") as f:
-      #infoLines is an array populated by the first six lines of the testCase file
-      infoLines = f.readlines()[0:6]
-      infoLines = list(map(str.strip,infoLines))
-
-      driver(parseFiles(infoLines))
 
 def parseFiles(inLineArray):
-  #1. test number or ID
-  #2. requirement being tested
-  #3. component being tested
-  #4. method being tested
-  #5. test input(s) including command-line argument(s)
-  #6. expected outcome(s)
+    # 1. test number or ID
+    # 2. requirement being tested
+    # 3. component being tested
+    # 4. method being tested
+    # 5. test input(s) including command-line argument(s)
+    # 6. expected outcome(s)
 
-  paren = '('
-  comm = '#'
-  defaultDriver = "driver"
+    paren = '('
+    comm = '#'
+    defaultDriver = "driver"
 
-  componentName = inLineArray[2]
+    componentName = inLineArray[2]
 
-  head, mid, tail = inLineArray[3].partition(paren)
-  funcName = head
+    head, mid, tail = inLineArray[3].partition(paren)
+    funcName = head
 
-  #print(inLineArray[4])
-  if '#' in inLineArray[4]:
-    head, mid, tail = inLineArray[4].partition(comm)
-  else:
-    head = inLineArray[4]
+    # print(inLineArray[4])
+    if '#' in inLineArray[4]:
+        head, mid, tail = inLineArray[4].partition(comm)
+    else:
+        head = inLineArray[4]
 
-  try:
-    inputVal = float(head.strip())
-  except:
-    print(head)
-    inputVal = 1.0
+    try:
+        inputVal = float(head.strip())
+    except:
+        print(head)
+        inputVal = 1.0
 
-  try:
-    driverName = inLineArray[6]
-  except:
-    driverName = defaultDriver
+    try:
+        driverName = inLineArray[6]
+    except:
+        driverName = defaultDriver
 
-  returnVal = [inLineArray[0], inLineArray[1], componentName, funcName, inputVal, inLineArray[5], driverName]
-  return returnVal
+    returnVal = [inLineArray[0], inLineArray[1], componentName, funcName, inputVal, inLineArray[5], driverName]
+    return returnVal
+
 
 def compare(oracle, actualOutput):
-  if oracle == actualOutput:
-    return True
-  else:
-    return False
+    if oracle == actualOutput:
+        return True
+    else:
+        return False
+
 
 def driver(info):
-  inFuncName = info[3]
-  inInputVal = info[4]
+    inFuncName = info[3]
+    inInputVal = info[4]
 
-  output = getattr(ui, inFuncName)(inInputVal)
-  print(output)
+    output = getattr(ui, inFuncName)(inInputVal)
+    #print(output)
+
 
 if __name__ == "__main__":
-  main()
+    main()
