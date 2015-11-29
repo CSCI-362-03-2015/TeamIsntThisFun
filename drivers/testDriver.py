f9e4484ae02ba69b15ad9b35ba2e547c73a65555
Zack <tod.zzx@gmail.com>
2015-11-29 14:04:46 -0500

    Create testDriver.py

---
+++ drivers/testDriver.py
@@ -0,0 +1,67 @@
+from TeamIsntThisFun.project.src.beets.beets import autotag
+from TeamIsntThisFun.project.src.beets.beets import ui
+from beets.autotag import hooks
+import beets
+
+def driverDefaultFunc(info):
+    """Calls the function specified in the test case specification file with the specified inputs, then returns the output."""
+    # info[2]. component being tested
+    # info[3]. method being tested
+    # info[4]. test input(s) including command-line argument(s)
+    #componentName = importlib.import_module(beets)
+
+    try:
+        inFuncName = info[3]
+    except: 
+        inFuncName = "human_bytes"
+    try:
+        inInputVal = info[4]
+    except:
+        pass
+
+    ## If there is only one input value, call the function with one argument
+    if (len(inInputVal) == 1):
+        try:
+            output = getattr(ui, inFuncName)(inInputVal[0])
+        except TypeError as e:
+            output = "TypeError"
+        #except InputError:
+        #    output = "InputError"
+        except:
+            output = "Error"
+    ## If there are two input values, call function with two arguments
+    elif (len(inInputVal) == 2):
+        if (info[2] == "autotag"):
+            #print(info[0] + "   rrr1")
+            try:
+                output = getattr(autotag.hooks, inFuncName)(inInputVal[0], inInputVal[1])
+            except TypeError as e:
+                output = "TypeError"
+            #except InputError:
+            #    output = "InputError"
+            except AssertionError as e:
+                output = "AssertionError"
+            except Exception, e:
+                print(str(e))
+                print(repr(e))
+                output = "Errorrr"
+        elif (info[2] == "ui"):
+            #print(info[0] + "   rrr2")
+            try:
+                #output = getattr(ui, "human_bytes")(None)
+
+                output = getattr(ui, inFuncName)(inInputVal[0], inInputVal[1])
+            except TypeError as e:
+                output = "TypeError"
+            #except InputError:
+            #    output = "InputError"
+            except AssertionError as e:
+                output = "AssertionError"
+            except Exception, e:
+                print(str(e))
+                print(repr(e))
+                output = "Errorrr2"
+    else:
+        output = "Some Error"
+
+    return output
